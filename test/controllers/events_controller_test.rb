1b7ddab932c26bd13cfb319bee3a383112e63218
Brett Morrison <brett.morrison@live.com>
2025-08-10 20:47:21 -0700

    fix/create fixtures

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "GET index returns success" do
+    get events_url
+    assert_response :success
+  end
+
+  test "GET show with real event returns success" do
+    get event_url(events(:fakechella))
+    assert_response :success
+  end
+
+  test "GET show with inexistent event returns 404" do
+    get "/events/69420"
+    assert_response :missing
+  end
+
+  test "request to nonexistent new action responds with 404" do
+    get "/events/new"
+    assert_response :missing
+  end
 end
