a85bc2f605f560ee7e9d4bfe8efd63e17de4b4cd
Brett Morrison <brett.morrison@live.com>
2025-08-10 22:38:40 -0700

    fix + add ArtistController tests

--- test/controllers/artist_events_controller_test.rb
+++ test/controllers/artist_events_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class ArtistEventsControllerTest < ActionDispatch::IntegrationTest
-  test "should get only:index" do
-    get artist_events_only: index_url
+  test "GET index returns success" do
+    get artist_events_url(artists(:one))
     assert_response :success
   end
 end
--- test/controllers/artists_controller_test.rb
+++ test/controllers/artists_controller_test.rb
@@ -1,13 +1,18 @@
 require "test_helper"
 
 class ArtistsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get artists_index_url
+  test "GET index returns success" do
+    get artists_url
     assert_response :success
   end
 
-  test "should get show" do
-    get artists_show_url
+  test "GET show returns success" do
+    get artist_url(artists(:one))
     assert_response :success
+  end
+
+  test "GET show with inexistent id returns 404" do
+    get "/artists/404"
+    assert_response :missing
   end
 end
